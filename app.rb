452f64fe9c1ebc2b35bcfaa1fa84088aef0d8f82
Tushar Pokle <tushar.pokle@gmail.com>
2011-11-06 23:55:24 +1100

    Only thing missing is the last url handling

---
+++ app.rb
@@ -0,0 +1,45 @@
+# dev hint: shotgun login.rb
+
+require 'rubygems'
+require 'sinatra'
+
+set :public_folder, File.dirname(__FILE__) + '/static'
+enable :sessions
+
+helpers do
+  def username
+    session[:identity] ? session[:identity] : 'Hello stranger'
+  end
+end
+
+before '/secure/*' do
+  if !session[:identity] then
+    session[:previous_url] = request['REQUEST_PATH']
+    @error = 'Sorry guacamole, you need to be logged in to do that'
+    halt erb(:login_form)
+  end
+end
+
+get '/' do
+  erb 'Can you handle a <a href="/secure/place">secret</a>?'
+end
+
+get '/login/form' do 
+  erb :login_form
+end
+
+post '/login/attempt' do
+  session[:identity] = params['username']
+  where_user_came_from = session[:previous_url] || '/'
+  redirect to where_user_came_from 
+end
+
+get '/logout' do
+  session.delete(:identity)
+  erb "<div class='alert-message'>Logged out</div>"
+end
+
+
+get '/secure/place' do
+  erb "This is a secret place that only <%=session[:identity]%> has access to!"
+end
