168ad45df9d4bcc6aa6bbcf7eca57b7aceaf4658
Milind <milind@hashcooki.es>
2014-04-21 22:50:55 +0530

    setting up the projects model and some other shit

--- app.rb
+++ app.rb
@@ -6,11 +6,32 @@ require 'sqlite3'
 
 configure :development do
 	DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/projects.db")
-	
-	DataMapper.auto_upgrade!
 end
 
+class Project
+
+	include DataMapper::Resource
+	
+	property :id, 			Serial
+	property :name,			String
+	property :description,  String
+	property :filename,		String
+	property :created_at,   DateTime
+	property :updated_at, 	DateTime
+	property :is_active,	Boolean
+	
+	def handle_upload( file )
+		path = File.join(Dir.pwd, "/public/projects", self.filename)
+		File.open(path, "wb") do |f|
+			f.write(file[:tempfile].read)
+		end
+	end
+end
+
+DataMapper.auto_upgrade!
+
 get '/' do
+	@projects = Project.all
   erb :home
 end
 
@@ -26,3 +47,32 @@ get '/services' do
 	erb "test"
 end
 
+get '/list' do
+	@page_title = "List Projects"
+	@projects = Project.all(:order => [:created_at.desc])
+	erb :list
+end
+
+get '/new' do
+	@page_title = "New Project"
+	erb :new
+end
+
+post '/create' do
+	@project = Project.new(params[:project])
+	@project.handle_upload(params[:image])
+	if @project.save
+		redirect "/show/#{@project.id}"
+	else
+		redirect('/list')
+	end
+end
+
+get '/show/:id' do
+	@project = Project.get(params[:id])
+	if @project
+		erb :show
+	else
+		redirect('/list')
+	end
+end
