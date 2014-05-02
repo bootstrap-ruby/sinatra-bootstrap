0db8d2719dfd1f14932e7795f1045108b8515cdc
Milind <milind@hashcooki.es>
2014-05-02 14:54:14 +0530

    Setting up design services view

--- app.rb
+++ app.rb
@@ -16,6 +16,7 @@ class Project
 	property :name,			String
 	property :description,  String
 	property :filename,		String
+	property :url,			String
 	property :created_at,   DateTime
 	property :updated_at, 	DateTime
 	
