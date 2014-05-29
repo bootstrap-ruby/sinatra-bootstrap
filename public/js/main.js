470765525511db88414cea5fd1a0174cde41f760
Milind <milind@hashcooki.es>
2014-05-29 15:49:15 +0530

    Setting up parallaxy stuff

--- public/js/main.js
+++ public/js/main.js
@@ -22,6 +22,20 @@ $(document).ready(function() {
 	});
 	
 	var $br = $('#browser');
+	var $ipad = $('#ipad');
+	var $iphone = $('#iphone');
+	var $soc = $('#social');
+	
+	$soc.css({
+		right: winW - 1170,
+		bottom: (winH - 231) / 2
+	});
+	
+	$ipad.add($iphone).css({
+		left: winW
+	});
+	
+
 //	$br.css({
 //		left: (winW - 1041) / 2
 //	});
@@ -34,10 +48,19 @@ $(document).ready(function() {
 		top: winH / 3
 	})
 	
+	var $sb1 = $('#services-block-1');
+	var $sb2 = $('#services-block-2');
+	var $sb3 = $('#services-block-3');
+	
+	$sb3.css({
+		left: (winW - 700) / 2,
+		bottom: (winH - 140) / 2
+	});
 	
 	$(window).scroll(function() {
 		var st = $(window).scrollTop();
 		var brLeft = st / 1.5;
+		console.log(st);
 		
 		if (st < winH) {
 			$('.cover').css({
@@ -58,33 +81,109 @@ $(document).ready(function() {
 		});
 
 		if (st < winH) {
-			$('#services-block-1').add('#services-block-1').css({ opacity: 0 });
+			$sb1.add($sb2).css({ opacity: 0 });
 		}
 
-		if (st > winH && st < (winH + 300)) {
-			$('#services-block-1').css({
+		if (st > winH) {
+			$sb1.css({
 				webkitTransform: 'translateY(' + (0 - (st / 10)) + 'px)',
 				opacity: 0 + ((st - winH) / 300)
 			});
+			
+			if (st < (winH + (winW / 1.95))) {
+				$ipad.css({
+					webkitTransform: 'translateX(' + (0 - ((st - winH) / 1.3)) + 'px)'
+				});
+				
+				$iphone.css({
+					webkitTransform: 'translateX(' + (0 - (st - winH)) + 'px)'
+				});
+				
+				$br.css({
+					webkitTransform: 'scale(' + (1 - ((st - winH) / 10000)) + ')'
+				});
+			}
+			
 		}
 		
-		if (st > (winH + 400)) {
-			$('#services-block-1').css({
-				opacity: 1 - ((st - (winH + 400)) / 300)
+		if (st > (winH + 700)) {
+			$sb1.css({
+				opacity: 1 - ((st - (winH + 700)) / 300)
 			});
 		}
 		
-		if (st > (winH + 800)) {
-			$('#services-block-2').css({
-				webkitTransform: 'translateY(' + (0 - (st / 10)) + 'px)',
-				opacity: 0 + ((st - (winH + 800)) / 300)
+		if (st > (winH + 1000)) {
+			var newt = st - (winH + 1000);
+			
+
+			$ipad.css({
+				bottom: 0 - (newt * 2)
+			});
+			
+			$iphone.css({
+				bottom: 0 - (newt * 1)
+			});
+			
+			
+			var matrix = $br.css('-webkit-transform').match(/-?[0-9\.]+/g);
+			
+			$br.css({
+				webkitTransform: 'scale(' + matrix[0] + ') translateX(' + (0 + newt) + 'px)'
+			});
+
+		}
+		
+		if (st > winH + 1400) {
+			var newt = st - (winH + 1400);
+			var scale;
+			
+			if ((0.5 + (newt / 400)) < 1) {
+				scale = (0.5 + (newt / 400))
+			} else {scale = 1}
+			
+			$sb2.css({
+				webkitTransform: 'translateY(' + (0 - (newt / 10)) + 'px)',
+				opacity: 0 + (newt / 300)
+			});
+			
+			$soc.css({
+				opacity: 0 + (newt / 400),
+				webkitTransform: 'scale(' + scale + ') translateY(' + (0 - (newt / 50)) + 'px)'
 			});
 		}
 		
-		if (st > (winH + 1400)) {
-			$('#services-block-2').css({
-				opacity: 1 - ((st - (winH + 1400)) / 300)
+		if (st > (winH + 2400)) {
+			var newt = st - (winH + 2400)
+			
+			var matrix = $sb2.css('-webkit-transform').match(/-?[0-9\.]+/g);
+			
+			$sb2.css({
+				webkitTransform: 'translateY(' + (matrix[5] - (newt * 1.5)) + 'px)'
 			});
+			
+			var matrix = $soc.css('-webkit-transform').match(/-?[0-9\.]+/g);
+			
+			$soc.css({
+				webkitTransform: 'scale(1) translateY(' + (matrix[5] - newt) + 'px)'
+			});
+		}
+		
+		if (st > winH + 3000 && st < winH + 3400) {
+			var newt = st - (winH + 3000);
+			
+			$sb3.css({
+				opacity: 0 + (newt / 100)
+			});
+			
+			
+			$sb3.find('#rule').width(newt);
+		}
+		
+		if (st > winH + 3500) {
+			$sb3.css({
+				webkitTransform: 'translateY(' + (0 - (st - (winH + 3500))) + 'px)'
+			});
+			
 		}
 		
 		
